ed0f5b7d5b3e0317fecc745578d9911f7ab2aa90
kelven <kelvenpersilva@gmail.com>
2022-12-20 21:41:58 -0300

    Criei uma tela chamada Consome API

---
+++ Projeto/Controllers/ApiController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.Controllers
+{
+    public class ApiController : Controller
+    {
+        public IActionResult ConsomeApi()
+        {
+
+            return View();
+        }
+    }
+}
