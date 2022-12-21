4bbff1d773b1a323b0a53eb495ab532a6fb7a2e1
kelven <kelvenpersilva@gmail.com>
2022-12-20 22:58:09 -0300

    API Implementada

--- Projeto/Controllers/ApiController.cs
+++ Projeto/Controllers/ApiController.cs
@@ -1,9 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis.Elfie.Serialization;
 
 namespace Projeto.Controllers
 {
     public class ApiController : Controller
     {
+
         public IActionResult ConsomeApi()
         {
 
---
+++ Projeto/Models/Api.cs
@@ -0,0 +1,11 @@
+﻿namespace Projeto.Models
+{
+    public class Api
+    {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public decimal Price { get; set; }
+            public string Category { get; set; }
+
+    }
+}
